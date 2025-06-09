6b37708d0885a9d623072197c5dbc7f8f8415b1e
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 00:13:41 +0800

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { removeFromCart } from '../redux/cartSlice';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../redux/cartSlice";
 
 const CartItem = ({ item }) => {
   const dispatch = useDispatch();
