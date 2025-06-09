f10964b664266af511b1ce6b8fecdd26718a6db0
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 00:23:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
-import React from "react";
+import React from "react";import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";       // Your Home component
+import AboutUs from "./AboutUs"; // Your AboutUs component
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<AboutUs />} />
+    </Routes>
+  );
+}
+
+export default App;
+
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
