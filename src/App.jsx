97e2237b955917dc1eb3c91a6602941c5a33125e
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 00:10:50 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import React from "react";
-import ProductList from "./components/ProductList";
-import CartItem from "./components/CartItem";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";  // <-- Import Router here
+import App from "./App";
+import store from "./redux/store";
 
-const sampleProducts = [
-  { name: "Aloe Vera", cost: "$10.00" },
-  { name: "Snake Plant", cost: "$15.00" },
-  { name: "Peace Lily", cost: "$20.00" },
-];
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const App = () => {
-  const cart = useSelector(state => state.cart.items);
-  const navigate = useNavigate();
-
-  const handleContinueShopping = () => {
-    navigate("/products"); // make sure this route is defined in your routes
-  };
-
-  return (
-    <div>
-      <ProductList products={sampleProducts} />
-      <CartItem cart={cart} onContinueShopping={handleContinueShopping} />
-    </div>
-  );
-};
-
-export default App;
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>  {/* Wrap your whole app with BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
