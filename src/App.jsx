e5510039d45de7c4a9f71c307086cbd8fc2c78f2
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 09:16:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import ProductList from "./ProductList";
-import CartItem from "./CartItem";
+import ProductList from "./components/ProductList.jsx";
+import CartItem from "./components/CartItem.jsx";
+import AboutUs from "./AboutUs.jsx";
 import { useSelector } from "react-redux";
 
 function App() {
@@ -14,11 +15,13 @@ function App() {
         <Link to="/">Products</Link> |{" "}
         <Link to="/cart">
           Cart <span style={{ background: "#eee", borderRadius: "50%", padding: "0 8px" }}>{totalItems}</span>
-        </Link>
+        </Link> |{" "}
+        <Link to="/about">About Us</Link>
       </nav>
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/cart" element={<CartItem />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </div>
   );
