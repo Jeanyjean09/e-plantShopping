c38adcfd26d57135508b12939026a9059933d3cf
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 20:53:39 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import ProductList from "./components/ProductList.jsx";
-import CartPage from "./components/CartPage.jsx";
-import AboutUs from "./AboutUs.jsx";
-import { useSelector } from "react-redux";
+
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
 
 function App() {
-  const cartItems = useSelector((state) => state.cart.items);
-  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+  
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  const handleHomeClick = () => {
+    setShowProductList(false);
+  };
 
   return (
-    <div>
-      <nav>
-        <Link to="/">Products</Link> |{" "}
-        <Link to="/cart">
-          Cart <span style={{ background: "#eee", borderRadius: "50%", padding: "0 8px" }}>{totalItems}</span>
-        </Link> |{" "}
-        <Link to="/about">About Us</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/about" element={<AboutUs />} />
-      </Routes>
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+         <div className="landing_content">
+         <h1>Welcome To Paradise Nursery</h1>
+          <div className="divider"></div>
+          <p>Where Green Meets Serenity</p>
+         
+          <button className="get-started-button" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+         </div>
+          <div className="aboutus_container">
+          <AboutUs/>
+          </div>
+          </div>
+
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList onHomeClick={handleHomeClick}/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
