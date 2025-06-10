e016bf8017b1e57266e9b2873da6034a2f1c1e88
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 09:25:00 +0800

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { removeFromCart } from "../redux/cartSlice";
+import { removeItem } from "../redux/cartSlice"; // Fix the import!
 
 const CartItem = ({ item }) => {
   const dispatch = useDispatch();
 
   const handleRemove = () => {
-    dispatch(removeFromCart({ id: item.id }));
+    dispatch(removeItem(item.name)); // Pass the name, as your reducer expects
   };
 
   return (
     <div>
       <h4>{item.name}</h4>
       <p>Quantity: {item.quantity}</p>
-      <p>Price: ${item.price}</p>
+      <p>Price: {item.cost}</p>
       <button onClick={handleRemove}>Remove</button>
     </div>
   );
