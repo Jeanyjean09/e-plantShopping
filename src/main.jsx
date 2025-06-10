6e6c22ec09f37cec61b70decb259310f131f605d
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 08:52:58 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom'; // <-- Import HashRouter
+import { HashRouter } from 'react-router-dom'; // <-- Make sure this is imported!
 import store from './store';
 import App from './App';
 
