d9a93122ccfa8d42d78cc65e7eb703c585051db4
Jeanyjean09 <96113767+Jeanyjean09@users.noreply.github.com>
2025-06-10 09:02:04 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom'; // <-- Make sure this is imported!
-import store from './store';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import store from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <HashRouter>
       <App />
